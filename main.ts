54079ab70dea938193d1f3e1d83409a58cfe14d3
jegyed50 <26919968+jegyed50@users.noreply.github.com>
2020-11-23 17:39:44 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,18 @@
 let counter = 0
 input.onButtonPressed(Button.A, function () {
     counter = 1
-    while (counter <= 6) {
+    while (counter <= 3) {
         basic.showNumber(counter)
         basic.pause(500)
         counter += 1
     }
-    basic.pause(1000)
+    basic.pause(500)
     basic.clearScreen()
+    KülsőKör()
+    BelsőKör()
+    Közepe()
 })
-input.onButtonPressed(Button.B, function () {
+function KülsőKör () {
     counter = 0
     while (counter <= 4) {
         led.plot(counter, 0)
@@ -34,7 +37,36 @@ input.onButtonPressed(Button.B, function () {
         counter += 1
         basic.pause(200)
     }
-})
+}
+function BelsőKör () {
+    counter = 1
+    while (counter <= 3) {
+        led.plot(counter, 1)
+        counter += 1
+        basic.pause(200)
+    }
+    counter = 1
+    while (counter <= 3) {
+        led.plot(3, counter)
+        counter += 1
+        basic.pause(200)
+    }
+    counter = 1
+    while (counter <= 3) {
+        led.plot(4 - counter, 3)
+        counter += 1
+        basic.pause(200)
+    }
+    counter = 1
+    while (counter <= 3) {
+        led.plot(1, 4 - counter)
+        counter += 1
+        basic.pause(200)
+    }
+}
+function Közepe () {
+    led.plot(2, 2)
+}
 basic.forever(function () {
 	
 })
