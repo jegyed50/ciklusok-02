46c552ee1f2bb2bed48ee4b775460173102cd251
jegyed50 <26919968+jegyed50@users.noreply.github.com>
2020-11-23 17:25:15 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,32 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(1000)
     basic.clearScreen()
 })
+input.onButtonPressed(Button.B, function () {
+    counter = 0
+    while (counter <= 4) {
+        led.plot(counter, 0)
+        counter += 1
+        basic.pause(200)
+    }
+    counter = 0
+    while (counter <= 4) {
+        led.plot(4, counter)
+        counter += 1
+        basic.pause(200)
+    }
+    counter = 0
+    while (counter <= 4) {
+        led.plot(4 - counter, 4)
+        counter += 1
+        basic.pause(200)
+    }
+    counter = 0
+    while (counter <= 4) {
+        led.plot(0, 4 - counter)
+        counter += 1
+        basic.pause(200)
+    }
+})
 basic.forever(function () {
 	
 })
